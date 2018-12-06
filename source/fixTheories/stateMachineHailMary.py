70f36f42ab68e32486d67e230941a2ea3874cf21
CelloCorgi <endremad@umich.edu>
2018-12-05 19:50:26 -0500

    added timestamp support and imporved logging

--- source/fixTheories/stateMachineHailMary.py
+++ source/fixTheories/stateMachineHailMary.py
@@ -1,7 +1,8 @@
 """
 This is a hail mary approach based off my state machine idea
 """
-import datetime
+from datetime import datetime
+import time
 import os
 import subprocess
 from copy import deepcopy
@@ -35,24 +36,36 @@ class StateMachine:
         to_look_for = stripped_error[stripped_error.find(last_element)+1:-1]
         print(stripped_error)
         print('to look for: ' + str(to_look_for))
-        if len(to_look_for) == 0:
-            print('YUP')
+        print(len(to_look_for))
+
         for i, e in reversed(list(enumerate(bad_input))):
+            if len(to_look_for) == 0 and len(e) == 0:
+                return i, 0, 0
+
             element_index = e.rfind(to_look_for)
             if element_index >= 0:
                 return (i, element_index, len(to_look_for))
+        
         # If not found, do a random one
         rand_row = random.randint(0, len(bad_input) - 1)
         return rand_row, 0, len(bad_input[rand_row])
 
-    def replace_element(self):
-        pass
+    def start_timer(self):
+        #self.start_time = datetime.utcnow() 
+        self.start_time = time.time()
+
+    def end_timer(self):
+        #self.end_time = datetime.utcnow()
+        self.end_time = time.time()
 
     def scan_input_for_type(self, bad_input, type_t, findAll=False):
         """
         This function returns the last element in the bad input to have that type (or all if findAll is true)
         """
         pass
+
+    def gen_random_string(self, length):
+        return ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(length))
 
     def random_mutation(self, bad_input):
         """
@@ -62,7 +75,7 @@ class StateMachine:
         # First, figure out which broad one to do
         # First pick a line to operate on
         if len(bad_input) == 0:
-            bad_input.append(''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(5)))
+            bad_input.append(self.gen_random_string(2))
             return bad_input
         
         operation_line = random.randint(0, len(bad_input) - 1)
@@ -72,7 +85,7 @@ class StateMachine:
 
         if len(split_version) <= 1:
             # Then here, we either add, delete, or swap
-            operation_choice = random.randint(0, 3)
+            operation_choice = random.randint(0, 4)
             if operation_choice == 0:
                 bad_input = []
             elif operation_choice == 1:
@@ -80,8 +93,9 @@ class StateMachine:
             elif operation_choice == 2:
                 bad_input.insert(operation_line, random.choice(bad_input + self.input_history[0]))
             elif operation_choice == 3:
-                bad_input.insert(operation_line, ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(5)))
-  
+                bad_input.insert(operation_line, self.gen_random_string(2))
+            elif operation_choice == 4:
+                bad_input[operation_line] = bad_input[operation_line] + ' ' + self.gen_random_string(2)
         else:
             operation_choice = random.randint(0, 3)
             if operation_choice == 0:
@@ -93,7 +107,7 @@ class StateMachine:
                 split_version[random.randint(0, len(split_version) - 1)] = random.choice(bad_input + self.input_history[0])
                 bad_input[operation_line] = ' '.join(split_version)
             elif operation_choice == 3:
-                split_version[random.randint(0, len(split_version) - 1)] = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(5))
+                split_version[random.randint(0, len(split_version) - 1)] = self.gen_random_string(2)
                 bad_input[operation_line] = ' '.join(split_version)        
         return bad_input
     
@@ -102,10 +116,9 @@ class StateMachine:
         This is the only method that should be called from outside this file
         It runs the overall fixer
         """
-        
         self.input_history = []
         self.num_tried += 1
-
+        self.found_solution = False
         # Convert the bad input into its modifiable datastructure form -> a list of lists
         print(session_config)
         bad_input = deepcopy(session_config["BadInput"])
@@ -113,29 +126,34 @@ class StateMachine:
         # Initialize the cache with the original bad, so we don't try the same result twice
         cache = {str(bad_input)}
 
-        # Record the starting time
-        self.start_time = datetime.datetime.now()
-        
-        # Loop through and try to find fixes
         num_fixes_found = 0
         num_probes_made = 0
         last_error_type = session_config["ErrorType"]
         last_error_message = session_config["ErrorMessage"]
         last_error_message = last_error_message[last_error_message.find(last_error_type) + len(last_error_type):]
         print(last_error_message)
-        self.log.write('The original bad input: {}\n'.format(bad_input))
 
+        self.log.write('ORIGINAL BAD INPUT: {}\n'.format(bad_input))
+        self.log.write('ORIGINAL ERROR: {} :-:-: {}\n'.format(last_error_type, last_error_message))
+        
+        # Record the starting time
+        self.start_timer()
+        
         while num_fixes_found < self.max_num_fix and num_probes_made < self.max_num_probe:
-            
+            if last_error_type == "" or last_error_type == ":":
+                num_probes_made = self.max_num_probe
+                self.log.write('NOTE: PROGRAM HAS NO ERROR MESSAGE, SO BEING SKIPPED\n')
+                return
+
             self.input_history.append(deepcopy(bad_input))
+            
             # Take a look at the error message associated with this guy
             # TODO: Deal with making deep copies of the bad input
             if last_error_type  == "ValueError":
-                print("I'm REALLY HERE")    
+                
+                #TODO: Don't have this fault location always be the last input line
                 # First look for values to unpack
                 if last_error_message.find('too many values to unpack') >= 0:
-                    
-                    # First split the last guy into tokens
                     split_last = bad_input[-1].strip().split()
                     bad_input[-1] = ' '.join(split_last[:-1])
 
@@ -159,7 +177,6 @@ class StateMachine:
 
                 elif last_error_message.find("could not convert string to float") >= 0:
                     # First, localize the error
-                    print(last_error_message)
                     if len(bad_input) == 0:
                         bad_input = [str(random.randint(0, 10)) + '.' + str(random.randint(0, 100))]
                     else:
@@ -168,18 +185,13 @@ class StateMachine:
                         bad_input[line] = bad_input[line][:i] + str(random.randint(0, 10)) + '.' + str(random.randint(0, 100))+ bad_input[line][i + l:]
 
             elif last_error_type == "EOFError":
-                
                 # In this case, repete the last line
                 # TODO: Make this work better in the future
                 if len(bad_input) > 0:
                     bad_input.append(random.choice(bad_input))
                 else:
                     bad_input.append(''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(5)))
-
             
-            elif last_error_type == "" or last_error_type == ":":
-                num_probes_made += 1
-                continue
             # If the input is in the cache at this point or nothing was done before, we need to keep trying to modify it
             while str(bad_input) in cache:
                 bad_input = self.random_mutation(bad_input)
@@ -199,30 +211,53 @@ class StateMachine:
             last_error_type, last_error_message, full_error = self.try_program(program_file_name, bad_input)
 
             if last_error_type is None:
-                self.log.write('Sucsessfully fixed error\n')
+                self.log.write('SOLVED THE ERROR\n')
                 num_fixes_found += 1
                 print("SOLVED THE ERROR")
-                print(self.minimize(program_file_name, bad_input))
+                self.found_solution = True
+                self.final_input = bad_input
+                self.minimized_input = self.minimize(program_file_name, deepcopy(bad_input))
                 self.num_fixed += 1
             else:
-                self.log.write('ERROR: {} :-:-: {}\n'.format(last_error_type, last_error_message))
+                self.log.write('STILL HAS ERROR: {} :-:-: {}\n'.format(last_error_type, last_error_message))
 
-            # Finally, either change the bad inputs, modify weights, or nothing and try again
-            print(bad_input)
-            print(session_config)
+        
+        # At this point, we have finished all of our probes for this scenario. It's time for cleanup
         # Record the ending time
-        elapsed_time = datetime.datetime.now() - self.start_time
-        self.log.write("The time elapsed was {}".format(elapsed_time))
+        self.end_timer()
+        self.wrap_up_scenario_log(session_config, num_probes_made)
         
         print("Num tried: {}".format(self.num_tried))
         print("Num fixed: {}".format(self.num_fixed))
+
+    def wrap_up_scenario_log(self, scenario_config, num_probes_made):
+        self.log.write('\nSCENARIO {} WRAP_UP:\n'.format(scenario_config['UniqueId']))
+        self.log.write('Student Ip: {}\n'.format(scenario_config['ip']))
+        self.log.write('Found Solution: {}\n'.format(self.found_solution))
+        self.log.write('Number of Probes made: {}\n'.format(num_probes_made))
+        self.log.write('Start time: {}\n'.format(self.start_time))
+        self.log.write('End time: {}\n'.format(self.end_time))
+        self.log.write('Student bad input start time: {}\n'.format(scenario_config['TimeStamp']))
+        print(self.end_time)
+        # See if the student had a good input after the bad input
+        for correct_input in scenario_config['CorrectInputs']:
+            if correct_input[1] > scenario_config['TimeStamp']:
+                self.log.write('Student next fix time: {}\n'.format(correct_input[1]))
+                break
+        self.log.write('Original bad input: {}\n'.format(scenario_config['BadInput']))
+        self.log.write('Original Error Type: {}\n'.format(scenario_config["ErrorMessage"]))
+        if self.found_solution:
+            self.log.write('Final correct fix: {}\n'.format(self.final_input))
+            self.log.write('Final correct minimized fix: {}\n'.format(self.minimized_input))
+        self.log.write('\n\n\n')
 
     def try_program(self, program_file_name, program_input):
         """
         This function runs the program with the specidied input and sees if it errors out or not
         """
         inputs = '\n'.join(program_input) + '\n'
-
+        
+        print('Program Input: {}'.format(program_input))
         # Now run the program:
         run_result = None
         error_last_line = None
@@ -253,7 +288,7 @@ class StateMachine:
 
         result, _, _ = self.try_program(program_file_name, program_input[:-1])
 
-        if result == None:
-            return program_input[:-1]
+        if result is not None:
+            return program_input
         else:
             return self.minimize(program_file_name, program_input[:-1])
