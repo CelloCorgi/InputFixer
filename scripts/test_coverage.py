63f265e4e8a172ecfdc1ffbee26477a13b7af3af
CelloCorgi <endremad@umich.edu>
2019-01-06 23:41:46 -0500

    modified test converagef -> made it so human_aesthetic is there

--- scripts/test_coverage.py
+++ scripts/test_coverage.py
@@ -6,7 +6,7 @@ import sys
 import coverage
 import subprocess
 import json
-
+import os
 
 def load_repair_log(log_file_path):
     """
@@ -42,12 +42,92 @@ def load_repair_log(log_file_path):
     log_file.close()
     return to_return
 
+def erase_coverage():
+    try:
+        subprocess.run(['coverage', 'erase'], check=True, timeout=30)
+    except Exception as e:
+        pass
+
+def report_coverage():
+    try:
+        report = subprocess.check_output(['coverage', 'report', '-m'])
+    except Exception as e:
+        return None
+
+    # Here, go through the things
+    print(report)
+
+def run_program_with_coverage(program_file_name, program_input):
+    """
+    This function runs coverage on the program with the given input
+    """
+    inputs = '\n'.join(program_input) + '\n'
+    
+    # now run the program
+    try:
+        subprocess.run(['coverage', 'run', '--include=*_code.py', '--branch',  program_file_name],
+                check=True,
+                timeout=60,
+                input=inputs,                                   
+                stdout=subprocess.PIPE,
+                universal_newlines=True)
+    except Exception as e:
+        pass
+
+
+def compare_coverage(result_info, useMinimizedFix=True):
+    """
+    This function sees which program -> the student or the machine gets better coverage
+    Returns None if there is no student correct answer or if there was no machine correct answer
+    """
+    if not "MachineFix" in result_info: return None
+    if not "StudentFixes" in result_info: return None
+    if len(result_info["StudentFixes"]) == 0: return None 
+    # If we get here, there was both a machine and a student fix
+    file_name = os.path.join('/home/endremad/Projects/Python_Tutor_Input_Experiments/PythonTutor_Input_Data_Sessions', result_info['Id'], result_info['Id'] + '_code.py') 
+    
+    
+    print(result_info)
+    
+    # Here, we get the coverage from the student fix
+    print('Student')
+    run_program_with_coverage(file_name, result_info["StudentFixes"][0][0])
+    
+    # And we analyze that coverage
+    report_coverage()
+    # And erase the coverage record
+    erase_coverage()
+
+    # Now we get the coverage from the machine fix
+    print('Machine')
+    run_program_with_coverage(file_name, result_info["MachineFix"])
+    
+    # And we analyze that coverage
+    report_coverage()
+
+    # And erase the coverage record
+    erase_coverage()
+
+    x = open(file_name, 'r')
+    y = ''
+    for line in x:
+        y+= line
+
+    print(y)
+    print()
+    print()
+    print()
+    return None
 
 if __name__ == "__main__":
 
     # First, load the log information
     scenario_results = load_repair_log(sys.argv[-1])
-    print(scenario_results)
+    #print(scenario_results)
+
     # Then calculate the coverage information
+    coverages = [compare_coverage(i, True) for i in scenario_results]
 
     # Then analyize the coverage information
+    print(coverages)
+
